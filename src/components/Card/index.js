cba300795f7dad868532b20fed1b88d4415b6b0c
Elmer C. Galo <elmer.galo@outlook.com>
2020-03-19 17:26:52 -0400

    Final changes to deploy app on github pages

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,9 +4,9 @@ import "./style.css";
 function Card(props){
 		return (
       <div className="card" onClick={props.clickShuffle}>
-        <img src={props.image} alt={props.image}/>
+        <img src={process.env.PUBLIC_URL + props.image} alt={props.image} />
       </div>
-    )
+    );
 }
 
 export default Card;
